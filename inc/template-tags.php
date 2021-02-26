2468a8658094c2f1fa60ef1d69987482bf35dc40
lelenei <zwpsky@gmail.com>
2021-02-26 09:53:37 +0800

    fix: header meta

--- inc/template-tags.php
+++ inc/template-tags.php
@@ -5,9 +5,8 @@ if (!function_exists("maskbook_news_posted_by")):
     function maskbook_news_posted_by() {
         printf(
             /* translators: 1: SVG icon. 2: Post author, only visible to screen readers. 3: Author link. */
-            '<span class="byline"><span class="screen-reader-text">%1$s</span><span class="author vcard"><a class="url fn n" href="%2$s">%3$s</a></span></span>',
+            '<span class="byline"><span class="screen-reader-text">%1$s </span><span class="author vcard">%2$s</span></span>',
             'Posted by' ,
-            esc_url( get_author_posts_url( get_the_author_meta( 'ID' ) ) ),
             esc_html( get_the_author() )
         );
 
@@ -18,21 +17,15 @@ endif;
 if (!function_exists("maskbook_news_posted_on")):
     function maskbook_news_posted_on() {
         $time_string = '<time class="entry-date published updated" datetime="%1$s">%2$s</time>';
-        if ( get_the_time( 'U' ) !== get_the_modified_time( 'U' ) ) {
-            $time_string = '<time class="entry-date published" datetime="%1$s">%2$s</time><time class="updated" datetime="%3$s">%4$s</time>';
-        }
-
+     
         $time_string = sprintf(
                 $time_string,
-                esc_attr( get_the_date( DATE_W3C ) ),
-                esc_html( get_the_date() ),
                 esc_attr( get_the_modified_date( DATE_W3C ) ),
                 esc_html( get_the_modified_date() )
         );
 
         printf(
-                '<span class="posted-on"><a href="%1$s" rel="bookmark">%2$s</a></span>',
-                esc_url( get_permalink() ),
+                '<span class="posted-on">%1$s</span>',
                 $time_string
         );
 
--- readme.txt
+++ readme.txt
@@ -3,6 +3,6 @@ Contributors: wordpressdotorg
 Tags:
 Requires at least: 4.9.6
 Tested up to: 5.6
-Stable tag: 1.9
+Stable tag: 1.0
 License: GPLv3 or later
 License URI: http://www.gnu.org/licenses/gpl-3.0.html
