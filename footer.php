d4fe2669ab8304e376f67ca117f4648feae7e05a
lelenei <zwpsky@gmail.com>
2021-02-26 14:56:08 +0800

    fix: mobile menu

--- footer.php
+++ footer.php
@@ -33,26 +33,26 @@
                         </nav>
                     </div>
                     <div class='footer-menu'>
-                        <nav>
+                        <?php
+                            wp_nav_menu(
+                                array(
+                                    'theme_location' => 'footer',
+                                    'container'      => false,
+                                    'depth'          => 1,
+                                    'items_wrap'     => '%3$s',
+                                )
+                                );
+                        ?>
+                        
+                        <li>
                             <?php
-                                wp_nav_menu(
-                                    array(
-                                        'theme_location' => 'footer',
-                                        'container'      => false,
-                                        'menu_class'     => 'footer-menu',
-                                        'depth'          => 1,
-                                    )
-                                    );
-                            ?>
-                            <li>
-                             <?php
-                                if ( function_exists( 'the_privacy_policy_link' ) ) {
-                                        the_privacy_policy_link( '', '<span role="separator" aria-hidden="true"></span>' );
-                                }
-                            ?>
-                            </li>
+                            if ( function_exists( 'the_privacy_policy_link' ) ) {
+                                    the_privacy_policy_link( '', '<span role="separator" aria-hidden="true"></span>' );
+                            }
+                        ?>
+                        </li>
+                        
 
-                        </nav>
                     </div>
                 </div> <!-- site-ifno -->
 
